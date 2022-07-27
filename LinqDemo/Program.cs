ee2a41adb52aeb8d6c8b2573b7e1954c9cb62bc2
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-27 15:16:59 +0200

    Add second Filter with query operators

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -8,12 +8,26 @@ namespace LinqDemo
         static void Main(string[] args)
         {
             var books = new BookRepostory().GetBooks();
+
+            //LINQ Extension Methods
             var cheapBooks = books
                                 .Where(b => b.Price < 10)
                                 .OrderBy(b =>  b.Title)
                                 .Select(b => b.Title);
 
             foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book);
+            }
+
+            //LINQ Query Operators
+            var cheaperBooks =
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b.Title;
+
+            foreach (var book in cheaperBooks)
             {
                 Console.WriteLine(book);
             }
