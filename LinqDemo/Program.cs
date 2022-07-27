402d13cc2d56f2dd133aa33ae6675415492533ea
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-27 14:49:43 +0200

    Update Filter

--- LinqDemo/Program.cs
+++ LinqDemo/Program.cs
@@ -8,11 +8,14 @@ namespace LinqDemo
         static void Main(string[] args)
         {
             var books = new BookRepostory().GetBooks();
-            var cheapBooks = books.Where(b => b.Price < 10);
+            var cheapBooks = books
+                                .Where(b => b.Price < 10)
+                                .OrderBy(b =>  b.Title)
+                                .Select(b => b.Title);
 
             foreach (var book in cheapBooks)
             {
-                Console.WriteLine(book.Title + " " + book.Price);
+                Console.WriteLine(book);
             }
         }
     }
