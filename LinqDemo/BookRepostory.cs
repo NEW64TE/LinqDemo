89ac15be2ef38bc84a2adc2685416a6a1e0b58e3
Eduard Gross <56992766+NEW64TE@users.noreply.github.com>
2022-07-27 13:37:28 +0200

    Create BookRepostory.cs

---
+++ LinqDemo/BookRepostory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqDemo
+{
+    public class BookRepostory
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "ADO.NET Basics", Price = 5},
+                new Book() {Title = "C# Advanced Topics", Price = 9.99f},
+                new Book() {Title = "JavaScript for Beginners", Price = 12},
+                new Book() {Title = "Clean Code", Price = 7},
+                new Book() {Title = "Windows PowerShell", Price = 8}
+            };
+        }
+    }
+}
